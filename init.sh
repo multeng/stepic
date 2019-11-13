eb51a02cfa96b68e238d2e05eba5dae38dad5cee
multeng <cenizo.kht@gmail.com>
2019-11-13 12:46:36 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,29 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+echo 'Creating directories'
+
+mkdir -p web
+
+cd web && mkdir -p uploads public etc
+
+cd public && mkdir -p js css img && cd ..
+
+echo 'Create config files for nginx and unicorn'
+
+touch etc/nginx.conf && touch etc/gunicorn.conf
+
+echo 'Create links, remove default config from /etc/nginx/sites-enabled/default'
+
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+
+sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
+
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+
 sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+
+sudo /etc/init.d/mysql start
+
+echo 'Done'
+
+exit
