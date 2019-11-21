c1489fe3f79c21df6a8315672ce41e7536b10964
Vladimir Kormakov <cenizo.kht@gmail.com>
2019-11-21 14:45:04 +0300

    changed configs and init.sh

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('', 'test'),
     path('login/', 'test', name='login'),
     path('signup/', 'test', name='signup'),
-    path('question/', include('qa.urls')),
+    path('question/<int:id>/', 'test', name='question'),
     path('ask/', 'test', name='ask'),
     path('popular/', 'test', name='popular'),
     path('new/', 'test', name='new'),
