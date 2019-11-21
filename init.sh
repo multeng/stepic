5bce2f78aefc6f06b97bd3512ceafd5e51abc175
Vladimir Kormakov <cenizo.kht@gmail.com>
2019-11-21 15:19:40 +0300

    changed configs and init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,24 @@
 echo 'Create links, remove default config from /etc/nginx/sites-enabled/default'
 
+sudo apt-get update
+
+sudo apt-get install -y python3.5
+
+sudo apt-get install -y python3.5-dev
+
+sudo unlink /usr/bin/python3
+
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+
 sudo pip3 install --upgrade pip
 
-sudo pip3 install django
+sudo pip3 install --upgrade django==2.1
 
-sudo pip3 install gunicorn
+sudo pip3 install --upgrade gunicorn
 
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+
+sudo cp /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ && sudo mv nginx.conf default
 
 sudo /etc/init.d/nginx restart
 
