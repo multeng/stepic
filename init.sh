1d611e0d2ec2e4171aae83b1e8b01a0820d3771c
Vladimir Kormakov <cenizo.kht@gmail.com>
2019-11-21 14:47:15 +0300

    changed configs and init.sh

--- init.sh
+++ init.sh
@@ -2,17 +2,14 @@ echo 'Create links, remove default config from /etc/nginx/sites-enabled/default'
 
 virtualenv --python=/usr/bin/python3.4 web/venv
 
-web/venv source web/venv/bin/activate
+source web/venv/bin/activate
 
-pip install --upgrade pip
+sudo pip3 install django
 
-pip install django
-
-pip install gunicorn
+sudo pip3 install gunicorn
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 
 sudo /etc/init.d/nginx restart
 
-source web/myvenv/bin/activate
 
